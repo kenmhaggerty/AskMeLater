6c6a5cb0c4ae099061873102e49e65f5df2f7a05
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-04 17:52:13 -0500

    Creates AMLFirebaseController.

---
+++ AskMeLater/Data Managers/Firebase/AMLFirebaseController.h
@@ -0,0 +1,21 @@
+//
+//  AMLFirebaseController.h
+//  AskMeLater
+//
+//  Created by Ken M. Haggerty on 3/4/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
+@interface AMLFirebaseController : NSObject
+
+@end
---
+++ AskMeLater/Data Managers/Firebase/AMLFirebaseController.m
@@ -0,0 +1,38 @@
+//
+//  AMLFirebaseController.m
+//  AskMeLater
+//
+//  Created by Ken M. Haggerty on 3/4/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "AMLFirebaseController.h"
+//#import "AKDebugger.h"
+//#import "AKGenerics.h"
+
+#pragma mark - // DEFINITIONS (Private) //
+
+@interface AMLFirebaseController ()
+@end
+
+@implementation AMLFirebaseController
+
+#pragma mark - // SETTERS AND GETTERS //
+
+#pragma mark - // INITS AND LOADS //
+
+#pragma mark - // PUBLIC METHODS //
+
+#pragma mark - // CATEGORY METHODS //
+
+#pragma mark - // DELEGATED METHODS //
+
+#pragma mark - // OVERWRITTEN METHODS //
+
+#pragma mark - // PRIVATE METHODS //
+
+@end
