2bc3aa6250939ccdaafb054381d6f58355558a1f
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-16 11:45:31 -0400

    Adds test UI to ViewController.

--- AskMeLater/View Controllers/ViewController.h
+++ AskMeLater/View Controllers/ViewController.h
@@ -6,10 +6,16 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
 #import <UIKit/UIKit.h>
+#import "NSObject+Basics.h"
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
 
 @interface ViewController : UIViewController
-
-
 @end
-
