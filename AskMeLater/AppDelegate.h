b60cdee18f866a9577a2ee019e8261d073d3ad65
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-16 11:45:11 -0400

    Adds AMLDataManager to AppDelegate; formats AppDelegate.

--- AskMeLater/AppDelegate.h
+++ AskMeLater/AppDelegate.h
@@ -6,14 +6,16 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
 #import <UIKit/UIKit.h>
 
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
 @property (strong, nonatomic) UIWindow *window;
-
-
-
-
 @end
-
