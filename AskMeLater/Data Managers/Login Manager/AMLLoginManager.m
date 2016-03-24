6eb302a2db472c216dc300b298b6e6ec13b55505
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-24 13:15:33 -0400

    Updates currentUser methods in AMLLoginManager.

--- AskMeLater/Data Managers/Login Manager/AMLLoginManager.m
+++ AskMeLater/Data Managers/Login Manager/AMLLoginManager.m
@@ -22,12 +22,17 @@
 NSString * const CurrentUserDidChangeNotification = @"kNotificationCurrentUserDidChange";
 
 @interface AMLLoginManager ()
-@property (nonatomic, strong) id <AMLUser_Editable> currentUser;
+@property (nonatomic, strong) id <AMLUser_PRIVATE> currentUser;
 
 // GENERAL //
 
 + (instancetype)sharedManager;
-+ (void)setCurrentUser:(id <AMLUser_Editable>)currentUser;
++ (void)setCurrentUser:(id <AMLUser_PRIVATE>)currentUser;
+
+// OTHER //
+
++ (id <AMLUser_Editable>)setCurrentUserUsingAuthData:(NSDictionary *)authData;
++ (void)updateUser:(id <AMLUser_Editable>)user withDictionary:(NSDictionary *)dictionary;
 
 @end
 
@@ -35,7 +40,7 @@ NSString * const CurrentUserDidChangeNotification = @"kNotificationCurrentUserDi
 
 #pragma mark - // SETTERS AND GETTERS //
 
-- (void)setCurrentUser:(id <AMLUser_Editable>)currentUser {
+- (void)setCurrentUser:(id <AMLUser_PRIVATE>)currentUser {
     [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetter tags:@[AKD_ACCOUNTS] message:nil];
     
     if ([AKGenerics object:currentUser isEqualToObject:_currentUser]) {
@@ -59,7 +64,8 @@ NSString * const CurrentUserDidChangeNotification = @"kNotificationCurrentUserDi
 + (id <AMLUser_Editable>)currentUser {
     [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter tags:@[AKD_ACCOUNTS] message:nil];
     
-    return [AMLLoginManager sharedManager].currentUser;
+    NSDictionary *authData = [AMLFirebaseController authData];
+    return [AMLLoginManager setCurrentUserUsingAuthData:authData];
 }
 
 + (void)signUpWithEmail:(NSString *)email password:(NSString *)password success:(void (^)(id <AMLUser_Editable>))successBlock failure:(void (^)(NSError *))failureBlock {
@@ -78,9 +84,7 @@ NSString * const CurrentUserDidChangeNotification = @"kNotificationCurrentUserDi
     [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeUnspecified tags:@[AKD_ACCOUNTS] message:nil];
     
     [AMLFirebaseController loginUserWithEmail:email password:password success:^(NSDictionary *userInfo) {
-        
-        id <AMLUser_Editable> currentUser = (id <AMLUser_Editable>)[AMLCoreDataController userWithEmail:email];
-        [AMLLoginManager setCurrentUser:currentUser];
+        id <AMLUser_Editable> currentUser = [AMLLoginManager setCurrentUserUsingAuthData:userInfo];
         successBlock(currentUser);
         
     } failure:^(NSError *error) {
@@ -114,10 +118,55 @@ NSString * const CurrentUserDidChangeNotification = @"kNotificationCurrentUserDi
     return _sharedManager;
 }
 
-+ (void)setCurrentUser:(id <AMLUser_Editable>)currentUser {
++ (void)setCurrentUser:(id <AMLUser_PRIVATE>)currentUser {
     [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetter tags:@[AKD_ACCOUNTS] message:nil];
     
     [AMLLoginManager sharedManager].currentUser = currentUser;
+}
+
+#pragma mark - // PRIVATE METHODS (Other) //
+
++ (id <AMLUser_Editable>)setCurrentUserUsingAuthData:(NSDictionary *)authData {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeUnspecified tags:@[AKD_DATA] message:nil];
+    
+    if (!authData) {
+        [AMLLoginManager setCurrentUser:nil];
+        return nil;
+    }
+    
+    NSString *userId = [authData objectForKey:FirebaseAuthKeyUID];
+    
+    id <AMLUser_PRIVATE> currentUser = [AMLLoginManager sharedManager].currentUser;
+    if (currentUser && [currentUser.userId isEqualToString:userId]) {
+        return currentUser;
+    }
+    
+    currentUser = [AMLCoreDataController userWithUserId:userId];
+    if (!currentUser) {
+        NSString *email = [authData objectForKey:FirebaseAuthKeyEmail];
+        currentUser = [AMLCoreDataController userWithUserId:userId email:email];
+    }
+    [AMLLoginManager updateUser:currentUser withDictionary:authData];
+    [AMLCoreDataController save];
+    [AMLLoginManager setCurrentUser:currentUser];
+    return currentUser;
+}
+
++ (void)updateUser:(id <AMLUser_Editable>)user withDictionary:(NSDictionary *)dictionary {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeUnspecified tags:@[AKD_DATA] message:nil];
+    
+    NSString *email = [dictionary objectForKey:FirebaseAuthKeyEmail];
+//    NSString *username;
+    NSString *profileImageURL = [dictionary objectForKey:FirebaseAuthKeyProfileImageURL];
+    
+    user.email = email;
+//    user.username = username;
+    if (profileImageURL) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:profileImageURL]];
+            user.avatar = [UIImage imageWithData:imageData];
+        });
+    }
 }
 
 @end
