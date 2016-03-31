b7915387937f297d0bec6a977f15f7174ce84d10
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-31 13:48:54 -0400

    Conveys question to AMLQuestionUI from AMLResultsViewController.

--- AskMeLater/View Controllers/AMLResultsViewController.m
+++ AskMeLater/View Controllers/AMLResultsViewController.m
@@ -14,11 +14,22 @@
 #import "AKDebugger.h"
 #import "AKGenerics.h"
 
+#import "AMLQuestionUIProtocol.h"
+
 #pragma mark - // DEFINITIONS (Private) //
 
 @interface AMLResultsViewController ()
 @property (nonatomic, strong) IBOutlet UIView *topView;
 @property (nonatomic, strong) IBOutlet UIView *bottomView;
+
+// OBSERVERS //
+
+- (void)addObserversToSurveyUI:(id <AMLSurveyUI>)surveyUI;
+- (void)removeObserversFromSurveyUI:(id <AMLSurveyUI>)surveyUI;
+
+// RESPONDERS //
+
+- (void)didSelectQuestion:(NSNotification *)notification;
 
 @end
 
@@ -44,6 +55,17 @@
 
 #pragma mark - // INITS AND LOADS //
 
+- (void)dealloc {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup tags:@[AKD_UI] message:nil];
+    
+    [self performBlockOnChildViewControllers:^(UIViewController *childViewController) {
+        if ([childViewController conformsToProtocol:@protocol(AMLSurveyUI)]) {
+            id <AMLSurveyUI> surveyUIController = (id <AMLSurveyUI>)childViewController;
+            [self removeObserversFromSurveyUI:surveyUIController];
+        }
+    }];
+}
+
 - (void)viewDidLoad {
     [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup tags:@[AKD_UI] message:nil];
     
@@ -51,7 +73,9 @@
     
     [self performBlockOnChildViewControllers:^(UIViewController *childViewController) {
         if ([childViewController conformsToProtocol:@protocol(AMLSurveyUI)]) {
-            ((id <AMLSurveyUI>)childViewController).survey = self.survey;
+            id <AMLSurveyUI> surveyUIController = (id <AMLSurveyUI>)childViewController;
+            surveyUIController.survey = self.survey;
+            [self addObserversToSurveyUI:surveyUIController];
         }
     }];
 }
@@ -69,10 +93,36 @@
 
 #pragma mark - // CATEGORY METHODS //
 
-#pragma mark - // DELEGATED METHODS //
+#pragma mark - // DELEGATED METHODS (AMLQuestionUI) //
 
 #pragma mark - // OVERWRITTEN METHODS //
 
-#pragma mark - // PRIVATE METHODS //
+#pragma mark - // PRIVATE METHODS (Observers) //
+
+- (void)addObserversToSurveyUI:(id<AMLSurveyUI>)surveyUI {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup tags:@[AKD_NOTIFICATION_CENTER] message:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didSelectQuestion:) name:AMLSurveyUIDidSelectQuestion object:surveyUI];
+}
+
+- (void)removeObserversFromSurveyUI:(id<AMLSurveyUI>)surveyUI {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup tags:@[AKD_NOTIFICATION_CENTER] message:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AMLSurveyUIDidSelectQuestion object:surveyUI];
+}
+
+#pragma mark - // PRIVATE METHODS (Responders) //
+
+- (void)didSelectQuestion:(NSNotification *)notification {
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeUnspecified tags:@[AKD_UI] message:nil];
+    
+    id <AMLQuestion> question = notification.userInfo[NOTIFICATION_OBJECT_KEY];
+    
+    [self performBlockOnChildViewControllers:^(UIViewController *childViewController) {
+        if ([childViewController conformsToProtocol:@protocol(AMLQuestionUI)]) {
+            ((id <AMLQuestionUI>)childViewController).question = question;
+        }
+    }];
+}
 
 @end
